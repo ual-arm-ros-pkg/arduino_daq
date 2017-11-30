a0ca9291436572644ae256fe2ee0de06b2e2aedf
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2017-11-30 10:14:37 +0100

    fix build against mrpt master

--- include/arduino_daq/ArduinoDAQ_LowLevel.h
+++ include/arduino_daq/ArduinoDAQ_LowLevel.h
@@ -35,7 +35,14 @@
 
 #pragma once
 
-#include <mrpt/hwdrivers/CSerialPort.h>
+#include <mrpt/version.h>
+#if MRPT_VERSION>0x190
+ #include <mrpt/comms/CSerialPort.h>
+ using mrpt::comms::CSerialPort;
+#else
+ #include <mrpt/hwdrivers/CSerialPort.h>
+ using mrpt::hwdrivers::CSerialPort;
+#endif
 
 #ifdef HAVE_ROS
 #include <ros/ros.h>
@@ -106,7 +113,7 @@ protected:
 	// Local class members:
 	std::string m_serial_port_name;
 	int         m_serial_port_baudrate;
-	mrpt::hwdrivers::CSerialPort m_serial;  //!< The serial COMMS object
+	CSerialPort m_serial;  //!< The serial COMMS object
 
 	// Local methods:
 	bool AttemptConnection();   //!< Returns true if connected OK, false on error.
